c5435cdb3ba880fa930de2f3856149f385c0c2e1
a1981611080 <58212953+a1981611080@users.noreply.github.com>
2020-04-10 13:10:51 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -14,60 +14,66 @@ rm -rf /tmp/v2ray
 install -d /usr/local/etc/v2ray
 cat << EOF > /usr/local/etc/v2ray/config.json
 {
-    "inbounds": [
-        {
-            "port": $PORT,
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "$UUID",
-                        "alterId": 64
-                    }
-                ],
-                "disableInsecureEncryption": true
-            },
-    "streamSettings": {
-      "network": "tcp",
+ "inbounds": [
+    {
+      "port": 80, //推荐80端口，更好地迷惑防火墙（好吧实际上并没有什么卵用
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID",
+            "level": 1,
+            "alterId": 64
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "tcp",
         "tcpSettings": {
-       "header": {
-         "type": "http",
-         "request": {
-        "version": "1.1",
-        "method": "GET",
-        "path": ["/"],
-        "headers": {
-          "Host": ["init.itunes.apple.com", "play.itunes.apple.com"],
-          "User-Agent": [
-         "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-         "Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_2 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/53.0.2785.109 Mobile/14A456 Safari/601.1.46"
-          ],
-          "Accept-Encoding": ["gzip, deflate"],
-          "Connection": ["keep-alive"],
-          "Pragma": "no-cache"
+          "header": { // header 这一项是关于数据包伪装的设置，可自定义合理的内容，但要确保服务器与客户端一致
+            "type": "http",
+            "response": {
+              "version": "1.1",
+              "status": "200",
+              "reason": "OK",
+              "headers": {
+                "Content-Type": ["application/octet-stream", "application/x-msdownload", "text/html", "application/x-shockwave-flash"],
+                "Transfer-Encoding": ["chunked"],
+                "Connection": ["keep-alive"],
+                "Pragma": "no-cache"
+              }
+            }
+          }
         }
-         },    
-         "response": {
-        "version": "1.1",
-        "status": "200",
-        "reason": "OK",
-        "headers": {
-          "Content-Type": ["application/octet-stream", "application/x-msdownload", "text/html", "application/x-shockwave-flash"],
-          "Transfer-Encoding": ["chunked"],
-          "Connection": ["keep-alive"],
-          "Pragma": "no-cache"
-        }
-         }
-       }
-        }
-   }
-        }
-    ],
-    "outbounds": [
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "blocked"
+    }
+  ],
+  "routing": {
+    "strategy": "rules",
+    "settings": {
+      "rules": [
         {
-            "protocol": "freedom"
+          "type": "field",
+          "ip": [
+            "geoip:private"
+          ],
+          "outboundTag": "blocked"
         }
-    ]
+      ]
+    }
+  }
+}
 }
 EOF
 
